f8284bdf14a435999e2bc52cad67fd3096a0d305
Nageshjagtap <nageshjagtap063@gmail.com>
2026-01-09 22:13:38 +0530

    render latest roadmap with details and interactive canvas

--- ai-career-coach/app/(routes)/ai-tools/ai-roadmap-agent/page.tsx
+++ ai-career-coach/app/(routes)/ai-tools/ai-roadmap-agent/page.tsx
@@ -26,12 +26,22 @@ const [error, setError] = useState<string | null>(null);
     fetchLatestRoadMap();
   }, []);
 
+     if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
-     <div>
-
-
-
-     </div>
+  <div className='grid grid-cols-1 md:grid-cols-3 gap-5 '>
+      <div className='border rounded-xl p-5'>
+          <h2 className='font-bold text-2xl'>{latestRoadMap?.roadMapData?.roadmapTitle}</h2>
+          <p className='mt-3 text-gray-500'><strong>Description:</strong><br></br> {latestRoadMap?.roadMapData?.description}</p>
+          <h2 className='mt-5 font-medium text-blue-600'>Duration: {latestRoadMap?.roadMapData?.duration}</h2>
+          <Button className='mt-5 w-full'>+ Create Another Roadmap</Button>
+      </div>
+      <div className='md:col-span-2 w-full h-[40vh]'>
+        <RoadMapCanvas initialNodes={latestRoadMap?.roadMapData?.initialNodes}  initialEdges={latestRoadMap?.roadMapData?.initialEdges} />
+      </div>
+      
+    </div>
   )
 };
 
